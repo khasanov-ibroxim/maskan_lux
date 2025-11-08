6663343776ad7c7dd9cdcb827d21d022131d840e
khasanov_ibroxim <xibroxim11@gmail.com>
2025-11-08 14:58:32 +0500

    first

--- src/page/login/users.jsx
+++ src/page/login/users.jsx
@@ -1,6 +1,8 @@
 export const users = [
-    { username: "Dovron", login: "dovron123", parol: "" },
-    { username: "Vali", login: "vali456", parol: "67890" },
-    { username: "Ali", login: "ali123", parol: "12345" },
-    { username: "Vali", login: "vali456", parol: "67890" },
+    { username: "Dovron", login: "dovron123", parol: "12345" },
+    { username: "Xusan", login: "xusan123", parol: "12345" },
+    { username: "Aziz", login: "aziz123", parol: "12345" },
+    { username: "Doniyor", login: "doniyor123", parol: "12345" },
+    { username: "Sevinch", login: "sevinch123", parol: "12345" },
+    { username: "Ibroxim", login: "ibroxim123", parol: "12345" },
 ];
