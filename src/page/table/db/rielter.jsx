0898ac64be704fd2f6b17ba4f007b3d5147b82d0
khasanov_ibroxim <xibroxim11@gmail.com>
2025-11-09 15:10:40 +0500

    first

--- src/page/table/db/rielter.jsx
+++ src/page/table/db/rielter.jsx
@@ -1,6 +1,6 @@
 export const Rielter = [
-    {name:"Aziz" , rielterExcelId:"https://script.google.com/macros/s/AKfycbz3LF3uLplFrTdOkWyRMVWTCtGzXA8eUX_VHNoUttxqBvCoWHNkWWx0DUkOTQ1NeNKjlA/exec" , rielterChatId:"-1003298985470" , themeId:65},
-    {name:"Davron" , rielterExcelId:"https://script.google.com/macros/s/AKfycbzHL4txmZQdbgkx7n3fFsJZPEv5on05GiOSHgmEooW4J4D-rFNM0-obJ-yM70zR7YEs/exec" , rielterChatId:"-1003298985470" , themeId:62},
+    {name:"Aziz" , rielterExcelId:"https://script.google.com/macros/s/AKfycbyVKd6wRAWhJ8q-VvAE7ZK0q5RGocDD2FIHyDcnNj1q746KLrjDj0Yn_OtIl_Z8Gs0S7w/exec" , rielterChatId:"-1003298985470" , themeId:65},
+    {name:"Davron" , rielterExcelId:"https://script.google.com/macros/s/AKfycbxfCxDD_UJ3iRnV387y-8cJJODPXAaUZu4uWh8Vb_U-6-2ePfYHoMXqTIfMgbkY9RCS/exec" , rielterChatId:"-1003298985470" , themeId:62},
     {name:"Xusan" , rielterExcelId:"https://script.google.com/macros/s/AKfycbxX2hsfesoNs96e25obI-zrGe-vJPQKy0JW3S8PQyZQelnAtjiou2o_G4KLUZVHQDnS5Q/exec" , rielterChatId:"-1003298985470" , themeId:63},
-    {name:"Doniyor" , rielterExcelId:"https://script.google.com/macros/s/AKfycbwROJSHVJYAhljDrPA62_x2ljO7hYyOkitu2a39ZvrFlzWLQLjmHoHphrf6YieAcfLucQ/exec" , rielterChatId:"-1003298985470" , themeId:83},
+    {name:"Doniyor" , rielterExcelId:"https://script.google.com/macros/s/AKfycbw7ZWxD2BR1vArjqswgp71ERHV1ljIec-7Ikg6VyzE0rE9hpTmcYjQZ4pshzdV6GNg8Ew/exec" , rielterChatId:"-1003298985470" , themeId:83},
 ]
